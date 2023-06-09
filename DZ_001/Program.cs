5efb3d15f42446f82d11b37e4b11dc9607c6be3e
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-06-09 21:46:56 +0300

    2 задачи сделаны

--- DZ_001/Program.cs
+++ DZ_001/Program.cs
@@ -1,19 +1,66 @@
-﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿
+// !!!РАСКОМЕНТИТЬ МНОГО СТРОК СРАЗУ - CTRL + /!!!
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-System.Console.WriteLine("Введите первое число");
-int number1 = Convert.ToInt32(System.Console.ReadLine());
-System.Console.WriteLine("Введите второе число");
-int number2 = Convert.ToInt32(System.Console.ReadLine());
-int max = 0;
-if(number1 > number2)
-{
-    max = number1;
-}
-else
-{
-    max = number2;
-}
-System.Console.WriteLine($"Максимальное число {max}");
+// System.Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(System.Console.ReadLine());
+// int max = 0;
+// int min = 0;
+// if(number1 > number2)
+// {
+//     max = number1;
+//     min = number2;
+// }
+// else
+// {
+//     max = number2;
+//     min = number1;
+// }
+// System.Console.WriteLine($"Максимальное число = {max}, а минимальное = {min}.");
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// System.Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите третье число");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+
+// if(b > max)
+// {
+//     max = b;
+// }
+// if(c > max)
+// {
+//     max = c;
+// }
+// System.Console.WriteLine($"Максимальное число = {max}");
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+
+
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
