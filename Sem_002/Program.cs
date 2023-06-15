28bf9d5e86d212169bde6dab43af68d8e4dcf8a1
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-06-15 14:37:29 +0300

    Привел в порядок Sem_002

--- Sem_002/Program.cs
+++ Sem_002/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
 
-
 // int Line(){
 //     int num = new Random().Next(10,100);
 //     Console.WriteLine($"Твое число {num}");
@@ -21,19 +20,18 @@
 
 
 //Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
-    // int num = new Random().Next(100,1000);
-    // System.Console.WriteLine($"Твое число {num}");
-    // int a = num % 10;
-    // int b = num / 100;
+
+// int num = new Random().Next(100, 1000);
+// System.Console.WriteLine($"Твое число {num}");
+// int a = num % 10;
+// int b = num / 100;
+// System.Console.WriteLine($"Результат  {b}{a}");
 
 
-// System.Console.WriteLine($"Результат  {b}{a}"); 
 
 
 
-// Напишите программу, которая будет принимать на вход два числа и выводить, 
-// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
-// то программа выводит остаток от деления.
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 
 // System.Console.WriteLine("Введите первое число");
 // int num1 = Convert.ToInt32(Console.ReadLine());
@@ -41,7 +39,7 @@
 // int num2 = Convert.ToInt32(Console.ReadLine());
 // if (num2 != 0 && num1 != 0){
 //     if (num2 % num1 == 0){
-//         System.Console.WriteLine("Число кратное");
+//         System.Console.WriteLine($"Число {num2} кратное {num1}");
 //     }
 //     else{
 //         System.Console.WriteLine("Остаток = " + num2 % num1);
@@ -51,19 +49,22 @@
 //     System.Console.WriteLine("Не вводи 0)))");
 // }
 
+
+
+
+
 // Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
-// void Line(){
+
+// int Line(){
 //     System.Console.WriteLine("Введите число");
 //     int num = Convert.ToInt32(Console.ReadLine());
 //     if (num % 7 == 0 && num % 23 == 0){
-//         System.Console.WriteLine("Четное");
+//         return 0;
 //     }
 //     else{
-//         System.Console.WriteLine("Не четное");;
+//         return 1;
 //     }
 // }
-
-
 // int line = Line();
 // if (line == 1){
 //     System.Console.WriteLine("Не кратно");
@@ -73,32 +74,24 @@
 // }
 
 
+
+
+
 // Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
 
 // System.Console.WriteLine("Введите первое число");
-// num1 = Convert.ToInt32(ReadLine());
-// System.Console.WriteLine("Введите первое число");
-// num2 = Convert.ToInt32(ReadLine());
-
-// if () {
-//     if (num2 != 0 && num1 != 0){
-//         num1
-//     }
-
-// }
-
-// void Enter(int num1, int num2){
-//     if (num1 == num2 * num2 || num2 == num1 * num1)
-//     {
-//         System.Console.WriteLine("Кратное");
-//     }
-//     else
-//     {
-//         System.Console.WriteLine("Не кратное");
-//     }
-// }
-
 // int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите первое число");
 // int num2 = Convert.ToInt32(Console.ReadLine());
-
-// Enter(num1, num2);
+// if (num2 == 0 || num1 == 0)
+// {
+//     System.Console.WriteLine("Айяйяй, чур 0 не писать");
+// }
+// else if (num1 == num2 * num2 || num2 == num1 * num1)
+// {
+//     System.Console.WriteLine("Число является квадратом");
+// }
+// else
+// {
+//     System.Console.WriteLine("Число не является квадратом");
+// }
