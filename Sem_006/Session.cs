765c70ed0389da1f63fcd5bcae383f1c00c7384e
Nikita Home <legathunter@mail.ru>
2023-06-27 23:25:24 +0300

    Решаю сессионнные задачи

--- Sem_006/Session.cs
+++ Sem_006/Session.cs
@@ -2,16 +2,62 @@
 // в которой каждый следующий член в три раза больше предыдущего,
 // т.е. {1, 3, 9, 27, 81,…}, причем последний член последовательности не должен превышать 1000.
 
+// void PrintArray(int [] printArray)
+// {
+//     for(int i = 0; i < printArray.Length; i++)
+//     {
+//     System.Console.Write($"{i}");
+//     if(i != printArray.Length - 1) System.Console.Write(", ");
+//     else System.Console.Write(".");
+//     }
+// }
 
+// int [] Posled()
+// {
+    
+//     int [] posl = new int [];
+//     for (int i = 0; i < 10; i++)
+//     {
+//         posl[i] *= 3;
+//     }
+//     return posl;
+// }
 
-
+// int [] a = Posled();
+// PrintArray(a);
 
 // Сформируйте массив целых чисел по алгоритму Фибоначчи: 
 // 1-й и 2-й элемент равны 1, а каждый последующий равен сумме двух предыдущих, 
 // т.е.: 1, 1, 2, 3, 5, 8, … . Найдите сумму и произведение его N членов.
 
+void PrintArray(int [] printArray)
+{
+    for(int i = 0; i < printArray.Length; i++)
+    {
+        System.Console.Write($"{i}");
+        if(i != printArray.Length - 1) System.Console.Write(", ");
+        else System.Console.Write(".");
+    }
+}
 
+int [] Feb(int size)
+{
 
+    // System.Console.WriteLine("Введите число: ");
+    // int n = Convert.ToInt32(Console.ReadLine());
+    int [] feb = new int [size];
+    feb[0] = 1;
+    feb[1] = 1;
+    for (int i = 2; i < size; i++)
+    {
+        feb[i] = feb[i - 1] + feb[i - 2];
+    }
+    return feb;
+}
+System.Console.WriteLine("Введите размер массива: ");
+int s = Convert.ToInt32(Console.ReadLine());
+int [] a = Feb(s);
+PrintArray(a);
 
 
 // Задан массив целых чисел A[20]. Создать другой массив целых чисел B[ ], 
