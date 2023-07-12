6775248420651ec8a3ed00ce06779f99593ca191
Nikita Prebyshevskiy <legathunter@mail.ru>
2023-07-12 19:37:22 +0300

    дз 9 полностью сделано

--- DZ_009/Program.cs
+++ DZ_009/Program.cs
@@ -27,17 +27,67 @@
 
 // int ShowNumSum(int n, int m)
 // {
-//     if (n <= m) return n + ShowNumSum(n + 1, m);
-//     else return 0;
+//     if (n > m)
+//     {
+//         int t = n;
+//         n = m;
+//         m = t;
+//     }
+//     if (n <= m)
+//     {
+//         if (n == m)
+//         {
+//             return n;
+//         }
+//         else
+//         {
+//             return n + ShowNumSum(n + 1, m);
+//         }
+//     }
+//     else
+//     {
+//         return 0;
+//     }
 // }
 
-// System.Console.WriteLine("Введите первое число:");
+// Console.WriteLine("Введите первое число:");
 // int a = Convert.ToInt32(Console.ReadLine());
-// System.Console.WriteLine("Введите второе число:");
+// Console.WriteLine("Введите второе число:");
 // int b = Convert.ToInt32(Console.ReadLine());
 // int summa = ShowNumSum(a, b);
-// System.Console.Write($"Cумма чисел = {summa}");
+// Console.Write($"Сумма чисел = {summa}");
 
-Задача 68*: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-m = 2, n = 3 -> A(m,n) = 9
-m = 3, n = 2 -> A(m,n) = 29
+
+
+
+
+// Задача 68*: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int Ackermann(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (m > 0 && n == 0)
+//     {
+//         return Ackermann(m - 1, 1);
+//     }
+//     else if (m > 0 && n > 0)
+//     {
+//         return Ackermann(m - 1, Ackermann(m, n - 1));
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+
+// System.Console.WriteLine("Введите число m");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите число n");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int result = Ackermann(m, n);
+// Console.WriteLine($"Результат функции Аккермана для m = {m} и n = {n}: {result}");
